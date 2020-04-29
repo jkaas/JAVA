2115bcd8c39d67f83f829b1b4a364402d4f03d25
73946 <73946@DESKTOP-13P5IQ6>
2020-04-29 23:02:50 +0800

    28

---
+++ src/two/eight/TestConstant.java
@@ -0,0 +1,19 @@
+package two.eight;
+
+public class TestConstant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 18;
+		final String NAME = "gaoqi";
+
+		final double PI = 3.14;
+		double r = 4;
+		double area = PI * r * r;
+		double circle = 2 * PI * r;
+		System.out.println("area = " + area);
+		System.out.println("circle = " + circle);
+	}
+
+}
+ 
--- src/two/six/TestVariable.java
+++ src/two/six/TestVariable.java
@@ -5,17 +5,24 @@ package two.six;
  *
  */
 public class TestVariable {
-
+	
+	int a; //成员变量，从属于对象；成员变量会自动被初始化
+	static int size; //静态变量，从属于类
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age;
-		age = 18;
+		{
+			int age;
+			age = 18;
+		}
 		int salary = 3000;
 		int gao = 13;
 		
 		
 		
 		System.out.println(gao);
+		int i;
+		// int j = i + 5;//编译出错，变量还未被初始化
 	}
 
 }
