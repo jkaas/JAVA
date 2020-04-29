88da932f93e8332ef5d61f5789c3b127a5ba390f
73946 <73946@DESKTOP-13P5IQ6>
2020-04-29 21:50:39 +0800

    1

--- src/BallGame2.java
+++ src/BallGame2.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class BallGame extends JFrame{
+public class BallGame2 extends JFrame{
 	
 	Image ball = Toolkit.getDefaultToolkit().getImage("images/ball.png");
 	Image desk = Toolkit.getDefaultToolkit().getImage("images/desk.jpg");
@@ -9,7 +9,7 @@ public class BallGame extends JFrame{
 	double x = 100;//小球的横坐标
 	double y = 100;//小球的纵坐标
 	
-	boolean right = true;//方向
+	double degree = 3.14/3;//方向
 	
 	//画窗口的方法
 	public void paint(Graphics g) {
@@ -17,24 +17,19 @@ public class BallGame extends JFrame{
 		g.drawImage(desk,0,0,null);
 		g.drawImage(ball,(int)x,(int)y,null);
 		
-		x = x + 1;
-	
-	
-	if(right) {
-		x = x + 10;
-	}else {
-		x = x - 10;
+		x = x + 10*Math.cos(degree);
+		y = y + 10*Math.sin(degree);
+		
+		if(y > 500 - 40 -30 || y < 40 + 40) {//500是桌子高度；40是桌子边框，30是球的直径；最后一个40是标题栏的高度
+			degree = -degree;
+		}
+		
+		if(x < 0|| x > 856 - 40) {
+			degree = 3.14 - degree;
+		}
+		
 	}
-	
-	if(x > 856 - 40 - 30) {//856是窗口宽度，40是桌子
-		right = false;
-	}
-	
-	if(x < 0) {
-		right = true;
-	}
-	
-	}
+		
 	//窗口加载
 	void launchFrame() {
 		setSize(856,500);
@@ -55,7 +50,7 @@ public class BallGame extends JFrame{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
-		BallGame game = new BallGame();
+		BallGame2 game = new BallGame2();
 		game.launchFrame();
 	}
 
