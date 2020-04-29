fca01ddc0cae9b680acbbeb6c4bae695eac19901
73946 <73946@DESKTOP-13P5IQ6>
2020-04-29 22:33:17 +0800

    24

---
+++ src/two/four/TestIdentifer.java
@@ -0,0 +1,26 @@
+package two.four;
+/**
+ * 测试标识符的用法
+ * @author 73946
+ *
+ */
+public class TestIdentifer {
+	//能力是练出来的，不是看书看出来的。对于初学者，一定要敲一遍
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a123 = 1;
+		//int 123ac = 2; // 数字是不能开头
+		int $a = 3;
+		int _abc = 4;
+		
+		//int #abc = 5;
+		
+		int 年龄 = 18; //可以使用汉字，但是不建议
+		
+		//int class = 2; //关键字不能作为标识符
+		
+		
+		
+	}
+
+}
