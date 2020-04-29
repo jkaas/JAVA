dda03aab59032088d2163a8a60d72fab7fd7b562
73946 <73946@DESKTOP-13P5IQ6>
2020-04-29 22:26:39 +0800

    23

---
+++ src/twenty/three/TestComment.java
@@ -0,0 +1,21 @@
+package twenty.three;
+
+/**
+ * 用来测试注释的用法(包含：多行注释、单行注释、文档注释）
+ * @author 李俊雄
+ * @version 1.0.1
+ */
+
+public class TestComment {
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args/*args是参数名，可以更改*/) {
+		// TODO Auto-generated method stub
+		System.out.println("测试注释"); //这是打印语句...
+	}
+	/*
+	 我是多行注释
+	 */
+}
