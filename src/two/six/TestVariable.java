ede77b314471cd16ad2e3984e19fd594f15d712d
73946 <73946@DESKTOP-13P5IQ6>
2020-04-29 22:38:17 +0800

    26

---
+++ src/two/six/TestVariable.java
@@ -0,0 +1,21 @@
+package two.six;
+/**
+ * 测试变量
+ * @author 73946
+ *
+ */
+public class TestVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age;
+		age = 18;
+		int salary = 3000;
+		int gao = 13;
+		
+		
+		
+		System.out.println(gao);
+	}
+
+}
