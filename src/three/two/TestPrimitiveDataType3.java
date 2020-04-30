d63548f4cbaba1fe7cb38b7291c53f48ff6fd5c5
73946 <73946@DESKTOP-13P5IQ6>
2020-04-30 10:43:10 +0800

    32

---
+++ src/three/two/TestPrimitiveDataType3.java
@@ -0,0 +1,25 @@
+package three.two;
+/**
+ * 测试字符类型
+ * @author 73946
+ *
+ */
+public class TestPrimitiveDataType3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'T';
+		char b = '尚';
+		char c = '\u0061';
+		System.out.println(c);
+		
+		//转义字符
+		System.out.println("" + 'a' + '\n' + 'b');
+		System.out.println("" + 'a' + '\t' + 'b');
+		System.out.println("" + 'a' + '\'' + 'b'); //a'b
+		
+		//String就是字符序列
+		String d ="abc";
+	}
+
+}
