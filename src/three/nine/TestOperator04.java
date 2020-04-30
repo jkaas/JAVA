ab9538e6c757cd57cab10f636eb012a918ae9fd7
73946 <73946@DESKTOP-13P5IQ6>
2020-04-30 11:49:45 +0800

    39

---
+++ src/three/nine/TestOperator04.java
@@ -0,0 +1,23 @@
+package three.nine;
+/**
+ * 位运算符
+ * @author 73946
+ *
+ */
+public class TestOperator04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b = 4;
+		System.out.println(a & b);
+		System.out.println(a | b);
+		System.out.println(a ^ b);
+		System.out.println(~a);
+		
+		int c = 3<<2;
+		System.out.println(c);
+		System.out.println(12 >> 1);
+	}
+
+}
