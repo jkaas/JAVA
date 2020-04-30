5101688bf5da5b02345a8581adb8c20eab26a18d
73946 <73946@DESKTOP-13P5IQ6>
2020-04-30 11:54:02 +0800

    40

---
+++ src/four/zero/TestOperator05.java
@@ -0,0 +1,20 @@
+package four.zero;
+/**
+ * 字符串运算符
+ * @author 73946
+ *
+ */
+public class TestOperator05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "3";
+		int b =4;
+		int c = 5;
+		char d = 'a';
+		System.out.println(a + b + c);
+		System.out.println(b + c + a);
+		System.out.println(d + 4); //94 + 4 = 101
+	}
+
+}
