ab686aa8d7ae49ac92ed0b96c46c246d7838b1cf
73946 <73946@DESKTOP-13P5IQ6>
2020-04-30 11:43:56 +0800

    38

---
+++ src/three/eight/TestOperator03.java
@@ -0,0 +1,25 @@
+package three.eight;
+/**
+ * 逻辑运算符
+ * @author 73946
+ *
+ */
+public class TestOperator03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b1 = true;
+		boolean b2 = false;
+		System.out.println(b1 & b2);
+		System.out.println(b1 | b2);
+		System.out.println(b1 ^ b2);
+		System.out.println(!b2);
+		
+		//短路
+		//int c = 3/0;
+		
+		boolean b3 = 1 > 2&2 < (3/0);//短路与，第一个操作数的值为false，则不需要在计算后面的操作数
+		System.out.println(b3);
+	}
+
+}
