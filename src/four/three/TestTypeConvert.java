70f270826c598a3e9c7e802d1bff1f239936bcd0
73946 <73946@DESKTOP-13P5IQ6>
2020-05-01 22:21:46 +0800

    测试自动类型转化

---
+++ src/four/three/TestTypeConvert.java
@@ -0,0 +1,25 @@
+package four.three;
+/**
+ * 测试自动类型转化
+ * @author 73946
+ *
+ */
+public class TestTypeConvert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 324;
+		long b = a;
+		double d = b;
+		// a = b;
+		//long e = 3.23F;
+		float f = 234324L;
+		
+		//特例
+		byte b2 = 123;
+		
+		
+		
+	}
+
+}
