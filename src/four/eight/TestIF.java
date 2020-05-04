b83785f78b5ad0c31f1dfde4b7a2931fcc447bb6
73946 <73946@DESKTOP-13P5IQ6>
2020-05-04 14:53:45 +0800

    测试if单选择结构

---
+++ src/four/eight/TestIF.java
@@ -0,0 +1,41 @@
+package four.eight;
+/**
+ * 测试if单选择结构
+ * @author 73946
+ *
+ */
+public class TestIF {
+	public static void main(String[] args) {
+		double d = Math.random(); //返回【0，1）之间的随机数
+		System.out.println(d);
+		
+		int i = (int)(6 * Math.random() + 1);
+		System.out.println(i);
+		if(i <= 3) {
+			System.out.println("小");
+			System.out.println("小小小");
+		}
+					
+		System.out.println("##############################");
+		//通过掷三个骰子看看今天的手气如何?
+		int j = (int)(6 * Math.random() + 1);
+		int k = (int)(6 * Math.random() + 1);
+		int count = i + j + k;
+		//如果三个骰子之和大于15，则手气不错
+		if(count > 15) {
+			System.out.println("今天手气不错");
+		}
+		//如果三个骰子之和在10到15之间，则手气一般
+		if(count >= 10 && count <= 15) {
+			System.out.println("今天手气很一般");
+		}
+		//如果三个骰子之和小于10，则手气不怎么样
+		if(count < 10) {
+			System.out.println("今天手气不怎么样");
+		}
+		System.out.println("得了" + count + "分");
+	}
+	
+	
+	
+}
