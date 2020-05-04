a224c1a058c135f577daeb5fe843a3ed293b7f16
73946 <73946@DESKTOP-13P5IQ6>
2020-05-04 20:44:29 +0800

    测试ifelse双选择结构

--- src/four/eight/TestIF.java
+++ src/four/eight/TestIF.java
@@ -15,25 +15,11 @@ public class TestIF {
 			System.out.println("小");
 			System.out.println("小小小");
 		}
-					
+		
 		System.out.println("##############################");
 		//通过掷三个骰子看看今天的手气如何?
-		int j = (int)(6 * Math.random() + 1);
-		int k = (int)(6 * Math.random() + 1);
-		int count = i + j + k;
-		//如果三个骰子之和大于15，则手气不错
-		if(count > 15) {
-			System.out.println("今天手气不错");
-		}
-		//如果三个骰子之和在10到15之间，则手气一般
-		if(count >= 10 && count <= 15) {
-			System.out.println("今天手气很一般");
-		}
-		//如果三个骰子之和小于10，则手气不怎么样
-		if(count < 10) {
-			System.out.println("今天手气不怎么样");
-		}
-		System.out.println("得了" + count + "分");
+		double area = Math.random();
+		
 	}
 	
 	
