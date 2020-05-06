cf9570e883fb5c6a641047cf924eacf84487526a
73946 <73946@DESKTOP-13P5IQ6>
2020-05-06 21:42:30 +0800

    测试for循环

---
+++ src/five/three/TestFor.java
@@ -0,0 +1,17 @@
+package five.three;
+/**
+ * 测试for循环
+ * @author 73946
+ *
+ */
+public class TestFor {
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 100;i++) {
+			sum += 1;
+		}
+	
+		System.out.println(sum);
+		
+	}
+}
